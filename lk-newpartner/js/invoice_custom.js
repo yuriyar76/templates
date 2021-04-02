3bd21dce026a89344572a466369cb62e815eaaf0
WEBPROG <elsnab76.1@gmail.com>
2021-04-02 20:33:47 +0300

    Массовый вызов курьера, массовая загрузка, номер накладной агента

--- lk-newpartner/js/invoice_custom.js
+++ lk-newpartner/js/invoice_custom.js
@@ -150,111 +150,181 @@ if($('a.print_btn_test').length>0) {
 		$(window).resize(function () {
 			$('#tableId').bootstrapTable('resetView');
 		});
-		$('#tableId').on('click', function(e){
+
+		$('#tableId').on('click', function(e) {
 
 			let el = e.target;
 			let id = el.id;
 			let s = id.match(/^update_\d+/i);
-			if(null !== s){
+			let n = id.match(/^num_edit_\d+/i);
+
+			if (null !== n) {
+				let id_val = id.replace('num_edit_', '');
+				let app_name = $('#NAME_' + id_val).text();
+				let el = $('#' + id).siblings();
+				let number = $(el[1]).text();
+				let alert_error = $('#edit_number .alert');
+				alert_error.html('');
+				alert_error.css("display","none");
+
+
+				let in_html = `<h5>Изменение № накладной ${number}</h5>
+				    <form id="edit_number_form" method="post">
+				        <input type="hidden" name="number_edit_num" value="${number}">
+				        <input type="hidden" name="id_edit_num" value="${id_val}"> 
+				        <input type="hidden" name="id_edit_app" value="${app_name}"> 
+				           <div class="form-group">
+							<label for="exampleInputNum">Введите новый номер накладной </label>
+							<input name="edit_number_new" type="text" class="form-control" id="exampleInputNum" >
+						  </div>  
+						  <button type="submit" class="btn btn-primary">Изменить</button>
+				    </form>
+			    <script>
+                   $('#edit_number_form').submit(function (e) {
+						e.preventDefault();
+						$('#edit_number_form button[type=submit]').attr('disabled', 'disabled');
+						let fields = $(this).serializeArray();
+						$.ajax({
+					    type: "POST",
+						dataType: "json",
+						url: "/tools/change_invoice.php?number=Y",
+						data: fields,
+						success: function(data)
+						{
+						console.log(data);
+						if(data.error){
+						let alert_error = $('#edit_number .alert');
+						alert_error.css("display","block");
+						alert_error.html('<p>' + data.error + '</p>')
+						console.log(data.error);
+						$('#edit_number_form button[type=submit]').removeAttr('disabled');
+						}else{
+						console.log(data.newnumber);
+						let modal_body = $('#edit_number .modal-body');
+                        modal_body.html('<h4 style="text-align: center">Обновляем...</h4>' +
+						'<div style="display: flex; flex-direction: row; justify-content: center">' +
+						'<img style="display:block" src="/bitrix/components/black_mist/newpartner.requests.v.2.1/templates/lk-newpartner/images/spinner.gif"' +
+						' alt="">' +
+						'</div>');
+						$('#edit_number').modal('hide');
+						}
+						
+						}
+						});
+						});
+                </script>         
+				`;
+				let modal_body = $('#edit_number .modal-body');
+				modal_body.html(in_html);
+				$('#edit_number').modal('show');
+			}
+
+			if (null !== s) {
 				$('#update_alert').modal('show');
-				var uid = $('#'+id).attr('data-uid');
-				var ukid = $('#'+id).attr('data-uk');
-				var inn_agent = $('#'+id).attr('data-inn');
+				var uid = $('#' + id).attr('data-uid');
+				var ukid = $('#' + id).attr('data-uk');
+				var inn_agent = $('#' + id).attr('data-inn');
 				var data = {
 					'id': id, 'uid': uid, 'uk': ukid, 'inn_agent': inn_agent,
 				};
-			}else{
-				return;
-			}
-
 			$.ajax({
 				type: "POST",
 				dataType: "json",
 				url: "/inapps/inapps_update.php",
 				data: data,
-				success: function(data){
+				success: function (data) {
 					var tableStart = '';
 					var id = data.ID;
-					if(data.PROPERTY_1059){
 
-						var dataJson = JSON.parse(data.PROPERTY_1059);
+					if (data.del === "Y") {
+						let id_rec = data.ID;
+						let rec = $(`#${id_rec}`);
+						console.log(rec);
+						rec.remove();
+					} else {
+						if (data.PROPERTY_1059) {
+							console.log(data);
+							var dataJson = JSON.parse(data.PROPERTY_1059);
 
-						$.each(dataJson, function (index, value) {
-							let ms = Date.parse(value.Date);
-							let myDate = moment(ms).format("DD-MM-YYYY HH:mm");
-							// console.log(myDate);
-							tableStart += `<tr>
+							$.each(dataJson, function (index, value) {
+								let ms = Date.parse(value.Date);
+								let myDate = moment(ms).format("DD-MM-YYYY HH:mm");
+								// console.log(myDate);
+								tableStart += `<tr>
                                         <td width="30%">${myDate}</td>
                                         <td width="35%">${value.Event}</td>
                                         <td width="35%">${value.Info}</td>
                                          </tr>`;
-						});
+							});
 
-					}else{
+						} else {
 
-						tableStart = `<tr>
+							tableStart = `<tr>
                                         <td width="30%">${data.PROPERTY_1061}</td>
                                         <td width="35%">${data.PROPERTY_1062}</td>
                                         <td width="35%">${data.PROPERTY_1062}</td>
                                          </tr>`;
 
+						}
+						$(`#PROPERTY_1059_MOD_${id}`).html(tableStart);
+						$(`#${id} #status_${id}`).text(data.PROPERTY_1062);
+
+						$(`#NAME_${id}`).text(data.NAME);
+						$(`#PROPERTY_1023_${id}_p`).text(data.PROPERTY_1023);
+						$(`#PROPERTY_1061_${id}`).text(data.PROPERTY_1061);
+						$(`#PROPERTY_1053_${id}`).text(data.PROPERTY_1053);
+						$(`#PROPERTY_1025_${id}`).text(data.PROPERTY_1025);
+						$(`#PROPERTY_1026_${id}`).text(data.PROPERTY_1026);
+						$(`#PROPERTY_1027_${id}`).text(data.PROPERTY_1027);
+						$(`#PROPERTY_1028_${id}`).text(data.PROPERTY_1028);
+						$(`#PROPERTY_1032_${id}`).text(data.PROPERTY_1032);
+						$(`#PROPERTY_1033_${id}`).text(data.PROPERTY_1033);
+						$(`#PROPERTY_1036_${id}`).text(data.PROPERTY_1036);
+						$(`#PROPERTY_1037_${id}`).text(data.PROPERTY_1037);
+						$(`#PROPERTY_1038_${id}`).text(data.PROPERTY_1038);
+						$(`#PROPERTY_1039_${id}`).text(data.PROPERTY_1039);
+						$(`#PROPERTY_1043_${id}`).text(data.PROPERTY_1043);
+						$(`#PROPERTY_1060_${id}`).text(data.PROPERTY_1060);
+						$(`#NAME_MOD_${id}`).html(`<h3>${data.NAME}</h3>`);
+						$(`#PROPERTY_1023_MOD_${id}`).html(`<h3>${data.PROPERTY_1023}</h3>`);
+						$(`#PROPERTY_1026_MOD_${id}`).html(`<strong>${data.PROPERTY_1026}</strong>`);
+						$(`#PROPERTY_1027_MOD_${id}`).html(`<strong>${data.PROPERTY_1027}</strong>`);
+						$(`#PROPERTY_1028_MOD_${id}`).html(`<strong>${data.PROPERTY_1028}</strong>`);
+						$(`#PROPERTY_1029_MOD_${id}`).html(`<strong>${data.PROPERTY_1029}</strong>`);
+						$(`#PROPERTY_1032_MOD_${id}`).html(`<strong>${data.PROPERTY_1032}</strong>`);
+						$(`#PROPERTY_1033_MOD_${id}`).html(`<strong>${data.PROPERTY_1033}</strong>`);
+						$(`#PROPERTY_1035_MOD_${id}`).html(`<strong>${data.PROPERTY_1035}</strong>`);
+						$(`#PROPERTY_1038_MOD_${id}`).html(`<strong>${data.PROPERTY_1038}</strong>`);
+						$(`#PROPERTY_1037_MOD_${id}`).html(`<strong>${data.PROPERTY_1037}</strong>`);
+						$(`#PROPERTY_1039_MOD_${id}`).html(`<strong>${data.PROPERTY_1039}</strong>`);
+						$(`#PROPERTY_1043_MOD_${id}`).html(`<strong>${data.PROPERTY_1043}</strong>`);
+						$(`#PROPERTY_1040_MOD_${id}`).html(`<strong>${data.PROPERTY_1040}</strong>`);
+						$(`#PROPERTY_1060_MOD_${id}`).html(`<strong>${data.PROPERTY_1060}</strong>`);
+						$(`#PROPERTY_1045_MOD_${id}`).html(`<strong>${data.PROPERTY_1045}</strong>`);
+						$(`#PROPERTY_1068_MOD_${id}`).html(`<strong>${data.PROPERTY_1068}</strong>`);
+						$(`#PROPERTY_1069_MOD_${id}`).html(`<strong>${data.PROPERTY_1069}</strong>`);
+						$(`#PROPERTY_1070_MOD_${id}`).html(`<strong>${data.PROPERTY_1070}</strong>`);
+						$(`#PROPERTY_1053_MOD_${id}`).html(`<strong>${data.PROPERTY_1053}</strong>`);
+						$(`#PROPERTY_1047_MOD_${id}`).html(`<strong>${data.PROPERTY_1047}</strong>`);
+						$(`#PROPERTY_1071_MOD_${id}`).html(`<strong>${data.PROPERTY_1071}</strong>`);
+						$(`#PROPERTY_1065_MOD_${id}`).html(`<strong>${data.PROPERTY_1065}</strong>`);
+						$(`#PROPERTY_1066_MOD_${id}`).html(`<strong>${data.PROPERTY_1066}</strong>`);
+						$(`#PROPERTY_1050_MOD_${id}`).html(`<strong>${data.PROPERTY_1050}</strong>`);
+						$(`#PROPERTY_1067_MOD_${id}`).html(`<h4>Спец. инструкции</h4><strong>${data.PROPERTY_1067}</strong>`);
 					}
-					$(`#PROPERTY_1059_MOD_${id}`).html(tableStart);
-					$(`#${id} #status_${id}`).text(data.PROPERTY_1062);
 
-					$(`#NAME_${id}`).text(data.NAME);
-					$(`#PROPERTY_1023_${id}`).text(data.PROPERTY_1023);
-					$(`#PROPERTY_1061_${id}`).text(data.PROPERTY_1061);
-					$(`#PROPERTY_1053_${id}`).text(data.PROPERTY_1053);
-					$(`#PROPERTY_1025_${id}`).text(data.PROPERTY_1025);
-					$(`#PROPERTY_1026_${id}`).text(data.PROPERTY_1026);
-					$(`#PROPERTY_1027_${id}`).text(data.PROPERTY_1027);
-					$(`#PROPERTY_1028_${id}`).text(data.PROPERTY_1028);
-					$(`#PROPERTY_1032_${id}`).text(data.PROPERTY_1032);
-					$(`#PROPERTY_1033_${id}`).text(data.PROPERTY_1033);
-					$(`#PROPERTY_1036_${id}`).text(data.PROPERTY_1036);
-					$(`#PROPERTY_1037_${id}`).text(data.PROPERTY_1037);
-					$(`#PROPERTY_1038_${id}`).text(data.PROPERTY_1038);
-					$(`#PROPERTY_1039_${id}`).text(data.PROPERTY_1039);
-					$(`#PROPERTY_1043_${id}`).text(data.PROPERTY_1043);
-					$(`#PROPERTY_1060_${id}`).text(data.PROPERTY_1060);
-
-					$(`#NAME_MOD_${id}`).html(`<h3>${data.NAME}</h3>`);
-
-					$(`#PROPERTY_1023_MOD_${id}`).html(`<h3>${data.PROPERTY_1023}</h3>`);
-					$(`#PROPERTY_1026_MOD_${id}`).html(`<strong>${data.PROPERTY_1026}</strong>`);
-					$(`#PROPERTY_1027_MOD_${id}`).html(`<strong>${data.PROPERTY_1027}</strong>`);
-					$(`#PROPERTY_1028_MOD_${id}`).html(`<strong>${data.PROPERTY_1028}</strong>`);
-					$(`#PROPERTY_1029_MOD_${id}`).html(`<strong>${data.PROPERTY_1029}</strong>`);
-					$(`#PROPERTY_1032_MOD_${id}`).html(`<strong>${data.PROPERTY_1032}</strong>`);
-					$(`#PROPERTY_1033_MOD_${id}`).html(`<strong>${data.PROPERTY_1033}</strong>`);
-					$(`#PROPERTY_1035_MOD_${id}`).html(`<strong>${data.PROPERTY_1035}</strong>`);
-					$(`#PROPERTY_1038_MOD_${id}`).html(`<strong>${data.PROPERTY_1038}</strong>`);
-					$(`#PROPERTY_1037_MOD_${id}`).html(`<strong>${data.PROPERTY_1037}</strong>`);
-					$(`#PROPERTY_1039_MOD_${id}`).html(`<strong>${data.PROPERTY_1039}</strong>`);
-					$(`#PROPERTY_1043_MOD_${id}`).html(`<strong>${data.PROPERTY_1043}</strong>`);
-					$(`#PROPERTY_1040_MOD_${id}`).html(`<strong>${data.PROPERTY_1040}</strong>`);
-					$(`#PROPERTY_1060_MOD_${id}`).html(`<strong>${data.PROPERTY_1060}</strong>`);
-					$(`#PROPERTY_1045_MOD_${id}`).html(`<strong>${data.PROPERTY_1045}</strong>`);
-					$(`#PROPERTY_1068_MOD_${id}`).html(`<strong>${data.PROPERTY_1068}</strong>`);
-					$(`#PROPERTY_1069_MOD_${id}`).html(`<strong>${data.PROPERTY_1069}</strong>`);
-					$(`#PROPERTY_1070_MOD_${id}`).html(`<strong>${data.PROPERTY_1070}</strong>`);
-					$(`#PROPERTY_1053_MOD_${id}`).html(`<strong>${data.PROPERTY_1053}</strong>`);
-					$(`#PROPERTY_1047_MOD_${id}`).html(`<strong>${data.PROPERTY_1047}</strong>`);
-					$(`#PROPERTY_1071_MOD_${id}`).html(`<strong>${data.PROPERTY_1071}</strong>`);
-					$(`#PROPERTY_1065_MOD_${id}`).html(`<strong>${data.PROPERTY_1065}</strong>`);
-					$(`#PROPERTY_1066_MOD_${id}`).html(`<strong>${data.PROPERTY_1066}</strong>`);
-					$(`#PROPERTY_1050_MOD_${id}`).html(`<strong>${data.PROPERTY_1050}</strong>`);
-					$(`#PROPERTY_1067_MOD_${id}`).html(`<h4>Спец. инструкции</h4><strong>${data.PROPERTY_1067}</strong>`);
 					$('#update_alert').modal('hide');
+
 					//console.log(data);
 				}
 			});
-
+	    	}
 		});
+
+
+
 	});
 
-	
 });
 
 
